27bc270955314d0cb0b760a63329a72a257274cd
Duarte Cancela <duartecancela@gmail.com>
2022-11-09 10:42:25 +0000

    update code

--- app/src/main/java/com/example/todoapp/ui/TodosFragment.kt
+++ app/src/main/java/com/example/todoapp/ui/TodosFragment.kt
@@ -5,17 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.todoapp.R
+import androidx.navigation.fragment.findNavController
+import com.example.todoapp.databinding.FragmentTodosBinding
 
 
 class TodosFragment : Fragment() {
+
+    private lateinit var binding: FragmentTodosBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_todos, container, false)
+        binding = FragmentTodosBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.addNewTodo.setOnClickListener {
+            findNavController().navigate(TodosFragmentDirections.actionTodosFragmentToNewTodoFragment())
+        }
+
     }
 
 }
